bf4263edf5deff5bec9dbd6ee8654ac35d4a1a36
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-01 00:07:04 +0600

    BasicVariablesInitialization task commit after testing for the fourth time

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,9 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first = 1, second = 10, third = 100;
 
         System.out.println(first);
         System.out.println(second);
