4d45a7630b02c03d79e9b4023d7dec3c015f0cdb
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-08-31 20:12:43 +0600

    Variables tasks commit

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int a = 10, b = a, c = b;
+        System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
