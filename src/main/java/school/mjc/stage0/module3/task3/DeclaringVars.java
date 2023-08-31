1f023b205dfdf1fc16958d6e67c30c1ad48c6984
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-08-31 20:34:58 +0600

    DeclaringVars task commit after testing

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10, b = a, c = b;
+        int a = 10, b = 10, c = 10;
         System.out.println(c);
     }
 }
