58fe9a466ba6d276fc2386c2d81afb08371c4036
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-01 00:02:49 +0600

    ReassigningValues task commit after testing for the third time

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,17 +2,13 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first = 1, second = 10, third = 100;
 
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
 
-        int linkToFirst = first;
-        int linkToSecond = second;
-        int linkToThird = third;
+        int linkToFirst = first, linkToSecond = second, linkToThird = third;
 
         first = 15;
         second = 6;
